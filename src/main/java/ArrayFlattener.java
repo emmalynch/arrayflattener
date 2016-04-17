94f5068f987fadb2cf679b7c4fd0dcb8cbf65c46
emmalynch <lynche4@gmail.com>
2016-04-17 12:41:02 +0100

    - Added completed array flattener and tests  - Downgraded testng version due to bug (https://youtrack.jetbrains.com/issue/IDEA-149750)  - Removed mockito dependency as it's not needed

--- src/main/java/ArrayFlattener.java
+++ src/main/java/ArrayFlattener.java
@@ -1,5 +1,30 @@
-/**
- * Created by emma on 17/04/16.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayFlattener {
+
+    /**
+     * Flattens an array of arbitrarily nested arrays of integers into a flat array of integers.
+     * e.g. [[1,2,[3]],4] → [1,2,3,4]
+     *
+     * @param unflattenedArray array of objects to accommodate ints or int arrays
+     * @return flattened array of ints
+     */
+    public static List<Integer> flatten(Object[] unflattenedArray) {
+        if(unflattenedArray == null){
+            return null;
+        }
+
+        List<Integer> flattenedArray = new ArrayList<>();
+
+        for (Object element : unflattenedArray) {
+            if (element instanceof Integer) {
+                flattenedArray.add((Integer)element);
+            } else {
+                flattenedArray.addAll(flatten((Object[]) element));
+            }
+        }
+        return flattenedArray;
+    }
+
 }
--- src/test/java/ArrayFlattenerTest.java
+++ src/test/java/ArrayFlattenerTest.java
@@ -1,5 +1,44 @@
-/**
- * Created by emma on 17/04/16.
- */
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static java.util.Collections.EMPTY_LIST;
+
 public class ArrayFlattenerTest {
+
+    @Test
+    public void testEmptyArray() {
+        List<Integer> result = ArrayFlattener.flatten(new Integer[0]);
+        Assert.assertEquals(result, EMPTY_LIST);
+    }
+
+    @Test
+    public void testNullArray() {
+        List<Integer> result = ArrayFlattener.flatten(null);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testSimpleArray() {
+        Integer[] testArray = {1, 2, 3, 4, 5};
+        List<Integer> result = ArrayFlattener.flatten(testArray);
+        Assert.assertEquals(result.toArray(), testArray);
+    }
+
+    @Test
+    public void test2DArray() {
+        Object[] testArray = {1, 2, 3, 4, new Object[]{ 5, 6, 7}};
+        List<Integer> result = ArrayFlattener.flatten(testArray);
+        Integer[] expectedResult = {1, 2, 3, 4, 5, 6, 7};
+        Assert.assertEquals(result.toArray(), expectedResult);
+    }
+
+    @Test
+    public void testComplexArray() {
+        Object[] testArray =  {1, new Object[]{2, 3}, 4, new Object[]{ 5, new Object[]{ new Object[]{ 6,new Object[]{7, 8}}}, 9}};
+        List<Integer> result = ArrayFlattener.flatten(testArray);
+        Integer[] expectedResult = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Assert.assertEquals(result.toArray(), expectedResult);
+    }
 }
